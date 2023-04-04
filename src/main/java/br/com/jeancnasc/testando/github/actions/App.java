64aa401f5beb5531b082e83275d509d9e2e922ec
Jean Carneiro do Nascimento <jeancnasc@hotmail.com>
2023-04-04 19:26:05 -0300

    commit inicial

---
+++ src/main/java/br/com/jeancnasc/testando/github/actions/App.java
@@ -0,0 +1,16 @@
+package br.com.jeancnasc.testando.github.actions;
+
+public class App
+{
+
+
+    public static void main( String[] args )
+    {
+        System.out.println(new App().mensagem());
+    }
+
+    public String mensagem() {
+        return "Hello World!";
+    }
+
+}
